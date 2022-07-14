b59f2d9dd6067f0721cd3b696a7bd2c603eebb8a
joseph sarpong <joseph-sarpong@hotmail.com>
2022-07-14 12:24:14 +0000

    implement carousel

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -2,6 +2,10 @@ $('.owl-carousel').owlCarousel({
     loop:true,
     margin:10,
     nav:false,
+    dots:false,
+    autoplay:true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause:true,
     responsive:{
         0:{
             items:1
